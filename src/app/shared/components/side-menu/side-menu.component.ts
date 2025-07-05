ffbe6e6899eb362e0c849c037beb907f370fb298
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2025-07-05 12:01:37 -0500

    Initial Commit Forms App

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item.interface';
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: ``,
+})
+export class SideMenuComponent {
+  public reactiveMenu: MenuItem[] = [
+    { title: 'Básicos', route: './reactive/basic' },
+    { title: 'Dinámicos', route: './reactive/dynamic' },
+    { title: 'Switchers', route: './reactive/switches' },
+  ];
+  public authMenu: MenuItem[] = [{ title: 'Registro', route: './auth' }];
+}
