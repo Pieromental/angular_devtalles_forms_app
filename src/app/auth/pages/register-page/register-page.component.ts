adc2a13bca8a4a5748caa5716b0c6865158724a0
PieroSalazar <psalazar@yawi.ai>
2025-07-19 22:44:56 -0500

    cambios

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
-
+  public myForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorsService
+  ) {
+    this.myForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.firstNameAndLastnamePattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+        [new EmailValidatorService()],
+      ],
+      userName: [
+        '',
+        [Validators.required, this.validatorService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+  isValidField(field: string): boolean | null {
+    return this.validatorService.isValidField(this.myForm, field);
+  }
+  onSubmit(): void {
+    console.log('X');
+  }
 }
